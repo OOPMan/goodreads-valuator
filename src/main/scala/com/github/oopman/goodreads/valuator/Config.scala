eb66cd69492e537e97a14aa5512421b42d8798a1
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2016-10-18 20:16:21 +0200

    Added scopt config structure.

---
+++ src/main/scala/com/github/oopman/goodreads/valuator/Config.scala
@@ -0,0 +1,41 @@
+package com.github.oopman.goodreads.valuator
+
+import scopt._
+
+case class Config(gooodReadsUserId: String = "", goodreadsAPIKey: String = "",
+                  shelf: String = "read", pageSize: String = "100")
+
+object Config {
+
+  val parser = new OptionParser[Config]("goodreadsValuator") {
+    head("goodreadsValuator", "0.1.0")
+
+    opt[Int]('u', "goodReadsUserId")
+      .required()
+      .text("GoodReads UserID for Shelves")
+      .action { (value, config) =>
+        config.copy(gooodReadsUserId = value.toString)
+      }
+
+    opt[String]('k', "goodReadsAPIKey")
+      .required()
+      .text("GoodReads Developer API key. See https://www.goodreads.com/api/keys")
+      .action { (value, config) =>
+        config.copy(goodreadsAPIKey = value)
+      }
+
+    opt[String]('s', "shelf")
+      .text("Shelf to valuate. Defaults to read")
+      .action { (value, config) =>
+        config.copy(shelf = value)
+      }
+
+    opt[Int]('p', "pageSize")
+      .text("Page size value for GoodReads API calls. Defaults to 100")
+      .action { (value, config) =>
+        config.copy(pageSize = value.toString)
+      }
+
+    help("help").text("Usage notes")
+  }
+}
