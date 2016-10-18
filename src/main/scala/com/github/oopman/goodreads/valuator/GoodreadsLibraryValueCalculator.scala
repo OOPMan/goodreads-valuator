a9a1bc34ea3fba55adc73f13b8d7a1c937dbbda7
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2016-10-18 20:17:56 +0200

    Updated GoodreadLibraryValueCalculator object to reflect changes. Broken.

--- src/main/scala/com/github/oopman/goodreads/valuator/GoodreadsLibraryValueCalculator.scala
+++ src/main/scala/com/github/oopman/goodreads/valuator/GoodreadsLibraryValueCalculator.scala
@@ -1,83 +1,103 @@
 package com.github.oopman.goodreads.valuator
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import com.typesafe.config._
-import dispatch._
 import dispatch.Defaults._
-import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import slick.driver.H2Driver.api._
-import com.github.oopman.goodreads.valuator.ISBNUtils._
 import com.typesafe.scalalogging.Logger
 import org.joda.money.{CurrencyUnit, Money}
 
+import scala.util.{Failure, Success}
+
 object GoodreadsLibraryValueCalculator extends App {
-
   val logger = Logger("com.github.oopman.goodreads.valuator.GoodreadsLibraryValueCalculator")
-  val config = ConfigFactory.load()
-  val browser = JsoupBrowser()
-  val goodreadsConfig = config.getConfig("goodreads")
-  val baseReviewService = url("https://www.goodreads.com/review/list")
-//  val db = Database.forConfig("h2disk1")
-
-  val reviewPages = getAllReviews(goodreadsConfig.getString("page_size"))
-  // TODO: Find a way to clean this up
-  val reviewNodes = for (reviewPage <- reviewPages) yield (for (reviews <- reviewPage) yield reviews \\ "review").flatten
-
-  // TODO: Find a way to clean this up
-  val isbnNodes = for (review <- reviewNodes) yield (for (reviewNode <- review) yield reviewNode \\ "isbn13").flatten
-  val collectedISBNs = isbnNodes.map { i => i.map { j => j.text }}
-  // TODO: Find a way to clean this up
-  val titleNodes = for (review <- reviewNodes) yield (for (reviewNode <- review) yield reviewNode \\ "title").flatten
-  val collectedTitles = titleNodes.map { i => i.map { j => j.text }}
-
-  val isbnToTitle = for {
-    isbn <- collectedISBNs
-    title <- collectedTitles
-  } yield Map(isbn zip title: _*)
+  val db = Database.forConfig("cache")
+  val httpResponse = TableQuery[HttpResponse]
 
   /**
-    * Group the ISBNs into chunks of 10. Each chunk of 10 will be processed in
-    * parallel before the next chunk of 10 is handled and so on until all the
-    * ISBNs have been processed
+    * Perform valuation
     *
-    * TODO: Allow for chunk size to be customizable
+    * @return
     */
-  val groupedISBNs = collectedISBNs.map(_.grouped(10).toList)
+  def valuate(config: Config) = {
+//    import com.github.oopman.goodreads.valuator.ISBNUtils._
+    val isbnUtils = new ISBNUtils(db)
 
-  val prices = groupedISBNs.map(getPricesForISBNChunks).flatMap(identity)
-  val isbnsAndPrices = for {
-    a <- collectedISBNs
-    b <- prices
-  } yield {
-    a zip b
+    val reviewPages = isbnUtils.getAllReviews(config)
+    // TODO: Find a way to clean this up
+    val reviewNodes = for (reviewPage <- reviewPages) yield (for (reviews <- reviewPage) yield reviews \\ "review").flatten
+
+    // TODO: Find a way to clean this up
+    val isbnNodes = for (review <- reviewNodes) yield (for (reviewNode <- review) yield reviewNode \\ "isbn13").flatten
+    val collectedISBNs = isbnNodes.map { i => i.map { j => j.text } }
+    // TODO: Find a way to clean this up
+    val titleNodes = for (review <- reviewNodes) yield (for (reviewNode <- review) yield reviewNode \\ "title").flatten
+    val collectedTitles = titleNodes.map { i => i.map { j => j.text } }
+
+    // TODO: Use this to output titles of unmatched ISBNs
+    val isbnToTitle = for {
+      isbn <- collectedISBNs
+      title <- collectedTitles
+    } yield Map(isbn zip title: _*)
+
+    /**
+      * Group the ISBNs into chunks of 10. Each chunk of 10 will be processed in
+      * parallel before the next chunk of 10 is handled and so on until all the
+      * ISBNs have been processed
+      *
+      * TODO: Allow for chunk size to be customizable
+      */
+    val groupedISBNs = collectedISBNs.map(_.grouped(10).toList)
+
+    val prices = groupedISBNs.map(isbnUtils.getPricesForISBNChunks).flatMap(identity)
+    val isbnsAndPrices = for {
+      a <- collectedISBNs
+      b <- prices
+    } yield {
+      a zip b
+    }
+
+    /**
+      * Map of Currency -> Seq of (ISBN, Some(Money)) tuples
+      *
+      * Additionally, "" maps to Seq of (ISBN, None) tuples
+      */
+    val currencyToISBNsAndPrices = isbnsAndPrices.map(_ groupBy {
+      case (_, Some(money)) => money.getCurrencyUnit.getCurrencyCode
+      case _ => ""
+    })
+
+    val ISBNsWithNoPrice = currencyToISBNsAndPrices.map(_.getOrElse("", IndexedSeq()).map(_._1).mkString(", "))
+
+    val currencyToSummedPrices = currencyToISBNsAndPrices.map(_.map {
+      case ("", _) => ("", Money.zero(CurrencyUnit.USD))
+      case (key, value) =>
+        (key, value.flatMap(_._2).reduce(_.plus(_)))
+    })
+
+    for (pricing <- currencyToSummedPrices; isbns <- ISBNsWithNoPrice) yield {
+      logger.info("Pricing amounts")
+      for ((currency, amount) <- pricing) {
+        if (currency != "") logger.info(s"$amount")
+      }
+      logger.info(s"ISBNs with no price: $isbns")
+      true
+    }
   }
 
-  /**
-    * Map of Currency -> Seq of (ISBN, Some(Money)) tuples
-    *
-    * Additionally, "" maps to Seq of (ISBN, None) tuples
-    */
-  val currencyToISBNsAndPrices = isbnsAndPrices.map(_ groupBy {
-    case (_, Some(money)) => money.getCurrencyUnit.getCurrencyCode
-    case _ => ""
-  })
+  Config.parser.parse(args, Config()) match {
+    case Some(config) =>
+      val init = db.run(DBIO.seq(httpResponse.schema.create))
+      val process = init.andThen {
+        case Success(_) => valuate(config)
+        case Failure(_) => valuate(config)
+      }
+      process onComplete { t =>
+        db.close()
+        System.exit(0)
+      }
+    case None =>
+      logger.error("No config")
+      db.close()
+      System.exit(-1)
 
-  val ISBNsWithNoPrice = currencyToISBNsAndPrices.map(_.getOrElse("", IndexedSeq()).map(_._1).mkString(", "))
-
-  val currencyToSummedPrices = currencyToISBNsAndPrices.map(_.map {
-    case ("", _) => ("", Money.zero(CurrencyUnit.USD))
-    case (key, value) =>
-      (key, value.flatMap(_._2).reduce(_.plus(_)))
-  })
-
-  val output = for (pricing <- currencyToSummedPrices; isbns <- ISBNsWithNoPrice) yield {
-    logger.info("Pricing amounts")
-    for ((currency, amount) <- pricing) {
-      if (currency != "") logger.info(s"$amount")
-    }
-    logger.info(s"ISBNs with no price: $isbns")
-    db.close()
-    System.exit(0)
   }
 }
